76c1a12d7b8f2358f1a896a9cfb89246cfcfa75c
Gulzar Hussain <gulzarhu88@gmail.com>
2026-04-27 01:17:00 +0530

    done

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -35,5 +35,9 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+# if __name__ == "__main__":
+#     app.run(port=5000, debug=True)
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0", port=port)
